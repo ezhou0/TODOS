5f17253614041bd9df3555f46281d59cd761aa23
Edwin <edwinzhou99@gmail.com>
2023-04-18 18:07:21 -0700

    add refresh and item manipulation

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ function getItems(){
     //console.log("value");
     return JSON.parse(value);
 }
+
+function setItems(items){
+    const itemsJson = JSON.stringify(items);
+
+    localStorage.setItems("todo-test" , itemsJson);
+}
+
+function addItem(){
+    items.unshift({
+        desciption: "",
+        completed: false,
+    });
+
+    setItems(items);
+    refreshList();
+}
+
+function refreshList(){ //sort items and update html for new data
+    ITEMS_CONTAINER.innerHTML = "";
+    for (const item of items){
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+        const desciptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        desciptionInput.value = item.desciption;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+refreshList();
