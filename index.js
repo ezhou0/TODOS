ab4911e17b562221c4c14b49475118d8bdf574eb
Edwin <edwinzhou99@gmail.com>
2023-04-18 17:59:03 -0700

    add getItems function

---
+++ index.js
@@ -0,0 +1,9 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+function getItems(){
+    const value = localStorage.getItem("todo") || "[]";
+    //console.log("value");
+    return JSON.parse(value);
+}
